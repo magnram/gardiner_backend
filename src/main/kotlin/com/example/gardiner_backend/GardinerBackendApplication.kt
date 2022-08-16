d9285451f75fd30848538713d3a31d75c7de954c
Magnus Ramm <magnus.ramm@bekk.no>
2022-08-16 09:44:02 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/gardiner_backend/GardinerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gardiner_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GardinerBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GardinerBackendApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/gardiner_backend/MainController.kt
@@ -0,0 +1,14 @@
+package com.example.gardiner_backend
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class MainController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello, world!"
+    }
+}
