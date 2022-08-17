11a48099887986c8a9b9fd931a1870981d55c63d
Magnus Ramm <magnus.ramm@bekk.no>
2022-08-17 16:02:42 +0200

    Add updates to backend

--- src/main/kotlin/com/example/gardiner_backend/Gardin.kt
+++ src/main/kotlin/com/example/gardiner_backend/Gardin.kt
@@ -1,10 +1,11 @@
 package com.example.gardiner_backend
 
 data class Gardin(
+    val id: Int,
     val pos1: Int,
-    val pos1Destination: Int,
+    var pos1Destination: Int,
     val pos2: Int,
-    val pos2Destination: Int) {
+    var pos2Destination: Int) {
 
     fun isActive(): Boolean {
         return pos1 != pos1Destination || pos2 != pos2Destination;
--- src/main/kotlin/com/example/gardiner_backend/MainController.kt
+++ src/main/kotlin/com/example/gardiner_backend/MainController.kt
@@ -1,27 +1,69 @@
 package com.example.gardiner_backend
 
 import com.google.gson.Gson
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.google.gson.reflect.TypeToken
+import org.springframework.web.bind.annotation.*
+import java.lang.reflect.Type
 
 
 @RestController
 class MainController {
 
-    val gardin1 = Gardin(100, 100, 0, 0)
-    val gardin2 = Gardin(100, 100, 0, 0)
-    val gardin3 = Gardin(100, 100, 0, 0)
-    val gardin4 = Gardin(100, 100, 0, 0)
+    val gardin1 = Gardin(0, 100, 80, 0, 0)
+    val gardin2 = Gardin(1, 100, 80, 0, 0)
+    val gardin3 = Gardin(2, 100, 100, 0, 0)
+    val gardin4 = Gardin(3, 100, 50, 0, 0)
 
-    @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello, world!"
+    var gardiner = listOf(gardin1, gardin2, gardin3, gardin4)
+
+    @CrossOrigin
+    @GetMapping("/reset")
+    fun reset(): String {
+        val gardin1 = Gardin(0, 100, 80, 0, 0)
+        val gardin2 = Gardin(1, 100, 80, 0, 0)
+        val gardin3 = Gardin(2, 100, 100, 0, 0)
+        val gardin4 = Gardin(3, 100, 50, 0, 0)
+
+        gardiner = listOf(gardin1, gardin2, gardin3, gardin4)
+        return "Resetted"
     }
-
     @CrossOrigin
     @GetMapping("/getAll")
     fun getAll(): String {
-        return Gson().toJson(listOf(gardin1, gardin2, gardin3, gardin4))
+        return Gson().toJson(gardiner)
     }
+
+    @CrossOrigin
+    @PostMapping("/updateOne")
+    fun updateOne(@RequestBody input: String): String {
+        println("---")
+        val updateOneInput = Gson().fromJson(input, UpdateOneInput::class.java)
+        val gardin = gardiner[updateOneInput.id]
+        println(gardin)
+        gardin.pos1Destination = updateOneInput.pos1Destination
+        gardin.pos2Destination = updateOneInput.pos2Destination
+        println(gardin)
+        return Gson().toJson(gardiner)
+    }
+
+    @CrossOrigin
+    @PostMapping("/updateAll")
+    fun updateAll(@RequestBody input: String): String {
+        val listType = object : TypeToken<List<UpdateOneInput>>() {}.type
+        val updateAllInput = Gson().fromJson<List<UpdateOneInput>>(input, listType)
+        println(gardiner)
+        updateAllInput.forEach {
+            val gardin = gardiner[it.id];
+            gardin.pos1Destination = it.pos1Destination
+            gardin.pos2Destination = it.pos2Destination
+        }
+
+
+
+
+        return Gson().toJson(gardiner)
+    }
+
+
+    data class UpdateOneInput (val id: Int, val pos1Destination: Int, val pos2Destination: Int)
 }
