03be89f5e5f217b1eb00ef89451ffd021ea8fa64
Magnus Ramm <magnus.ramm@bekk.no>
2022-09-01 12:20:06 +0200

    Added cors

--- src/main/kotlin/com/example/gardiner_backend/yr/YrController.kt
+++ src/main/kotlin/com/example/gardiner_backend/yr/YrController.kt
@@ -1,9 +1,6 @@
 package com.example.gardiner_backend.yr
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("/yr")
@@ -14,6 +11,7 @@ class YrController(val yrService: YrService) {
         return "Hello World!"
     }
 
+    @CrossOrigin
     @GetMapping("/getWeather")
     fun getYr(@RequestParam lng: Double, @RequestParam lat: Double): String? {
         return yrService.getWeatherUrl(lng, lat)
